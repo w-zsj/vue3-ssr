05033c09297f65ab5ceb75fcfcc76e59952e8ffd
zhangsanjun <zhangsanjun@eyee.com>
2022-04-23 12:17:53 +0800

    ee

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ import i18n from "./i18n";
 import SvgIcon from "./components/SvgIcon.vue";
 import mitt from "mitt";
 export function createApp() {
-  const app = createSSRApp(Page);
-  const router = createRouter();
-  const emitter = mitt();
-  const $bus: any = {
-    on: emitter.on,
-    emit: emitter.emit,
-    off: emitter.off,
-  };
+  let app = createSSRApp(Page),
+    router = createRouter(),
+    emitter = mitt(),
+    $bus: any = {
+      on: emitter.on,
+      emit: emitter.emit,
+      off: emitter.off,
+    };
   app.config.globalProperties.$bus = $bus;
   app.use(router).use(store).use(i18n).component("svg-icon", SvgIcon);
   return { app, router };
